8b7e8e29d98a5f763554679b51952a93ee7eaa92
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2022-06-28 14:06:01 +0200

    first working version

---
+++ 00_scripts/functions/wss_calculations.py
@@ -0,0 +1,65 @@
+import numpy as np
+def calculate_damage(lu_block, depth_block, indices, dmg_table_landuse, dmg_table_general, pixel_factor):
+    #GAMMA DEPTH CALCULATION
+
+    #Interpoleren van het diepteraster en de gamma array.
+    ysize, xsize = depth_block.shape
+    k, j = np.meshgrid(np.arange(xsize), np.arange(ysize))
+
+    xp=np.array(dmg_table_general['inundatiediepte'])
+
+    #Zoek voor de diepte_array de dichtst bijliggende index wanneer vergeleken met xp.
+    #Ookal gaat dit 'links', we willen de index hebben daarom krijg index_onder -1
+    index_boven = np.searchsorted(xp, depth_block, side='left')
+    index_boven[index_boven==len(xp)]-=1
+
+    index_onder=index_boven.copy()-1
+    index_onder[index_onder==-1]=0
+
+    #Stacked array zijn de gamma waarden van de inundatiediepte. Heeft dezelfde vorm als depth_block.
+    # En dan x lagen gelijk aan len(xp) 
+    stacked_array = []
+    for i_depth in range(len(xp)):
+        lookup = np.asarray([dmg_table_landuse[i].gamma_inundatiediepte[i_depth] for i in range(0,255)])
+        stacked_array.append(lookup[lu_block]) # Max directe schade.
+        # stacked_array.append(np.take(lookup, lu_block))
+    stacked_array = np.stack(stacked_array)
+
+    #Met de index arrays kunnen we nu de onder en bovenwaarde ophalen om vervolgens linear te interpoleren
+    # tussen deze twee waarden. Hiermee krijgen we de gamma_inundatiediepte.
+    #(y2-y1)/(x2-x1) * (x-x1) + y1
+    np.seterr(invalid='ignore') #Delen door 0 is niet te voorkomen. Met het mask berekenen we de waarden hier.
+    mask = index_onder==index_boven #dy/dx is hier 0, we pakken de index zoals die is.
+    y1=stacked_array[index_onder, j, k]
+    y2=stacked_array[index_boven, j, k]
+    gamma_inundatiediepte = ((y2 - y1) /           \
+        (xp[index_boven] - xp[index_onder])) * (depth_block-xp[index_onder]) + y2
+    gamma_inundatiediepte[mask] = y1[mask]
+
+
+    #DAMAGE CALCULATION
+
+    #Apply lookup table. Change the landuse with direct damage.
+    def calculate_damge_direct(i):
+        """schade = max. directe schade · γdiepte · γduur · γseizoen + indirecte schade per dag · hersteltijd"""
+        lu = dmg_table_landuse[i]
+        return lu.direct_gem * lu.gamma_inundatieduur[indices['duur']] * lu.gamma_maand[indices['maand']] * lu.direct_eenheid_factor
+
+    def calculate_damge_indirect(i):
+        #TODO hoe werkt inundatiediepte door op indirecte schade?
+        """schade = max. directe schade · γdiepte · γduur · γseizoen + indirecte schade per dag · hersteltijd"""
+        lu = dmg_table_landuse[i]
+        return lu.indirect_gem * lu.gamma_herstelperiode[indices['herstelperiode']] * lu.indirect_eenheid_factor
+
+    # Create lookuptable. Landuse value will be replaced by calculated damage. 
+    lookup_direct = np.asarray([calculate_damge_direct(i) for i in range(0,255)])
+    lookup_indirect =  np.asarray([calculate_damge_indirect(i) for i in range(0,255)])
+
+    # Max directe schade.
+    damage_direct = lookup_direct[lu_block] #same as np.take(lookup_direct, lu_block), .take seems slower.
+    damage_indirect = lookup_indirect[lu_block]
+
+    #Damage
+    ##schade = max. directe schade · γdiepte · γduur · γseizoen + indirecte schade per dag · hersteltijd
+    ## damage is per m2. Multiply by pixelfactor to get damage per pixel.
+    return (damage_direct + damage_indirect) * gamma_inundatiediepte * pixel_factor
---
+++ 00_scripts/functions/wss_loading.py
@@ -0,0 +1,53 @@
+import configparser
+
+def read_dmg_table_config(cfg_file) -> dict:
+    """Read damagetable. Returns dictionary with keys corresponding to the 
+    landuse index. """
+
+    class Landuse_damagetable():
+        """Damagetable for one land use"""
+        def __init__(self, section):
+            """Section is part of the configparser. [i.items(sect) for i in parser.sections()]"""
+            self.section = dict(section)
+            for key in self.section:
+                if '[' in self.section[key]:
+                    setattr(self, key, eval(self.section[key]))
+                else:
+                    try:
+                        setattr(self, key, int(self.section[key]))
+                    except:
+                        setattr(self, key, self.section[key])
+
+            #Omrekenen van eenheden.
+            if self.direct_eenheid == '/ha': #TODO buiten hele loop halen want hoeft maar 1 keer.
+                self.direct_eenheid_factor = 1/10000
+            elif self.direct_eenheid == '/m2':
+                self.direct_eenheid_factor = 1
+            if self.indirect_eenheid == '/m2/dag':
+                self.indirect_eenheid_factor = 1
+        
+
+        def __repr__(self):
+            return f"""{self.omschrijving}"""
+
+    parser = configparser.ConfigParser()
+    parser.read(filenames=cfg_file)
+
+    dmg_table_landuse={}
+
+    #Read file
+    for sect in parser.sections():
+        if sect !='algemeen':
+            dmg_table_landuse[int(sect)] = Landuse_damagetable(parser.items(sect))
+        else:
+            dmg_table_general = dict(parser.items(sect))
+            for key in dmg_table_general: #List in string to list.
+                dmg_table_general[key] = eval(dmg_table_general[key])
+
+    #Fill missing values with dummy values. Dummy is defined as landuse=0
+    for i in range(0,255): #Careful. Landuse value should be max 254
+        if i not in dmg_table_landuse:
+            dmg_table_landuse[i] = dmg_table_landuse[0]
+
+    return dmg_table_landuse, dmg_table_general
+
